355f6db3935c2cee38a5872be20e21cc26d449dd
Rupa Mahindrakar <rupamahindrakar08@gmail.com>
2024-08-07 00:27:14 +0530

    responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,27 @@ import Home from "./pages/Home/Home"
 import Cart from "./pages/Cart/Cart"
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder"
 import Navbar from "./components/navbar/Navbar"
+import Footer from "./components/Footer/Footer"
+import { useState } from "react"
+import LoginPopup from "./components/LoginPopup/LoginPopup"
 
 
 function App() {
+  const [showLogin, setShowLogin] = useState(false)
 
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/order" element={<PlaceOrder/>} />
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        {showLogin? <LoginPopup /> : <></>}
+        <Navbar setShowLogin={setShowLogin}/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder/>} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,31 @@
 import "./Navbar.css"
 import { assets } from "../../assets/assets"
 import { useState } from "react"
+import { Link } from "react-router-dom"
 
-const Navbar = () => {
-    const [menu, setMenu] = useState("menu")
+const Navbar = ({ setShowLogin }) => {
+    const [menu, setMenu] = useState("home")
 
-  return (
-    <div className="navbar">
-        {/* <img src={assets.sugar} className="logo" alt="" /> */}
-        <p>SavorStreet.</p>
-        <ul className="navbar-menu">
-            <li onClick={()=> setMenu("home")} className={menu === "home" ? "active" : ""}>home</li>
-            <li onClick={()=> setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</li>
-            <li onClick={()=> setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>mobile-app</li>
-            <li onClick={()=> setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>contact us</li>
-        </ul>
-        <div className="navbar-right">
-            <img src={assets.search_icon} alt="" />
-            <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="" />
-                <div className="dot"></div>
+    return (
+        <div className="navbar">
+            {/* <img src={assets.sugar} className="logo" alt="" /> */}
+            <p>SavorStreet.</p>
+            <ul className="navbar-menu">
+                <Link to="/" onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>home</Link>
+                <a href="#explore-menu" onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</a>
+                <a href="#app-download" onClick={() => setMenu("download")} className={menu === "download" ? "active" : ""}>download</a>
+                <a href="#footer" onClick={() => setMenu("contact")} className={menu === "contact" ? "active" : ""}>contact</a>
+            </ul>
+            <div className="navbar-right">
+                <img src={assets.search_icon} alt="" />
+                <div className="navbar-search-icon">
+                    <img src={assets.basket_icon} alt="" />
+                    <div className="dot"></div>
+                </div>
+                <button onClick={() => setShowLogin(true)}>sign in</button>
             </div>
-            <button>sign in</button>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
